ac63f70fbed0ac034a450a64f70e9063461188db
Domnyus <domnyus@gmail.com>
2023-10-09 15:11:58 -0300

    0.1

---
+++ src/main/java/com/domnyus/todo/user/UserModel.java
@@ -0,0 +1,11 @@
+package com.domnyus.todo.user;
+
+import java.sql.Date;
+
+public class UserModel 
+{
+    public String username;
+    public String password;
+    public char role;
+    public Date created_;
+}
