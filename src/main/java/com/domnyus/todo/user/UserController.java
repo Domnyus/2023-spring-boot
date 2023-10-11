325edff334fe760bfffd8c06311b320b58df009f
Domnyus <domnyus@gmail.com>
2023-10-11 20:45:08 -0300

    2023-10-10

--- src/main/java/com/domnyus/todo/user/UserController.java
+++ src/main/java/com/domnyus/todo/user/UserController.java
@@ -1,5 +1,8 @@
 package com.domnyus.todo.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/user")
-public class UserController 
-{
+public class UserController {
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void Create(@RequestBody UserModel userModel)
-    {
-        System.out.println(userModel.username);
+    public ResponseEntity Create(@RequestBody UserModel userModel) {
+        var result = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (result != null) {
+            System.out.println("User not unique");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists!");
+        }
+        var userCreated = this.userRepository.save(userModel);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
     }
 }
--- src/main/java/com/domnyus/todo/user/UserModel.java
+++ src/main/java/com/domnyus/todo/user/UserModel.java
@@ -1,11 +1,31 @@
 package com.domnyus.todo.user;
 
-import java.sql.Date;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name="_users")
 public class UserModel 
 {
-    public String username;
-    public String password;
-    public char role;
-    public Date created_;
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID Id;
+
+    @Column(unique=true)
+    private String username;
+
+    
+    private String password;
+    private char role;
+
+    @CreationTimestamp
+    private LocalDateTime created;
 }
